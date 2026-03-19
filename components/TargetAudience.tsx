ec736ef1a50b0be402e8bcd8fbe8b0407a46d017
Cotet Mihai <cotet.mihaita.cornel@gmail.com>
2026-03-19 20:02:22 +0200

    refactor(TargetAudienceSection): optimization changes

--- components/TargetAudience.tsx
+++ components/TargetAudience.tsx
@@ -1,114 +1,107 @@
-import Image from "next/image";
-
-import { Card } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
+import {Card} from "@/components/ui/card";
 
-export default function TargetAudience() {
+export default function TargetAudienceSection() {
     return (
-        <section className="py-24 px-6 mx-10 lg:px-8 lg:h-screen flex flex-col overflow-hidden">
-            <div className="max-w-7xl mx-auto w-full h-full flex flex-col">
+        <section className={'flex flex-col py-24 px-10 w-full max-w-7xl mx-auto h-screen'}>
+            {/* Header */}
+            <div className={'flex flex-col sm:flex-row md:items-end justify-between items-center w-full gap-6'}>
+                <div className={'flex flex-col gap-2 max-w-2xl'}>
+                    <h2 className={' text-3xl md:text-5xl lg:text-6xl font-serif tracking-tight font-bold'}>
+                        Cui ne adresăm ?
+                    </h2>
+                    <p className={'text-muted-foreground text-sm'}>
+                        Reciclarea nu e aceeași pentru toți. Fiecare are rolul său,
+                        iar noi avem soluții personalizate pentru companii,
+                        instituții și persoane fizice.
+                    </p>
+                </div>
 
-                {/* Header */}
-                <div className="bg-[#f3f3f5] flex flex-col md:flex-row justify-between items-start mb-12 shrink-0 p-10 rounded-4xl">
-                    <div>
-                        <h2 className="font-serif text-primary text-6xl lg:text-5xl font-bold tracking-tight">
-                            Cui ne adresăm?
-                        </h2>
-                        <p className="text-muted-foreground mt-3 max-w-2xl">
-                            Reciclarea nu e aceeași pentru toți. Fiecare are rolul său,
-                            iar noi avem soluții personalizate pentru companii,
-                            instituții și persoane fizice.
-                        </p>
-                    </div>
-
-                    <Button  variant={'outline'} className={'h-12 px-6 rounded-full font-medium transition-all duration-300 hover:scale-105 mt-10 ml-auto'}>
+                <div>
+                    <Button
+                        variant={'outline'}
+                        className={'p-6 rounded-full font-bold hover:scale-105 duration-300'}
+                    >
                         <Link href={'/despre-noi'}>
                             Află mai mult
                         </Link>
                     </Button>
                 </div>
+            </div>
 
-                <div className="flex-1 grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 gap-6 min-h-0">
+            {/* Cards */}
+            <div className={'flex-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10'}>
+                {/* Card 1 */}
+                <Card className={'relative lg:row-span-2 overflow-hidden rounded-3xl group h-100 lg:h-full'}>
+                    <Image
+                        src={'/images/phone-2.png'}
+                        alt={'Secțiune despre producatori'}
+                        fill
+                        className={'object-cover object-[50%_60%] group-hover:scale-105 transition duration-500'}
+                    />
+                    <div className="absolute inset-0 bg-gray-400/10" />
+                    <div className={'absolute inset-0 flex flex-col items-center px-8'}>
+                        <h3 className={'text-2xl md:text-4xl font-semibold uppercase pt-8 font-serif text-foreground'}>
+                            PRODUCATORI
+                        </h3>
+                        <p className={'text-sm text-muted-foreground mt-2'}>
+                            Introduci echipamente electrice și electronice pe piață?
+                            Ai nevoie de un partener pentru conformare legală.
+                        </p>
+                    </div>
+                </Card>
 
-                    {/* Card 1 */}
-                    <Card className="relative lg:row-span-2 overflow-hidden rounded-3xl group h-[400px] lg:h-full">
-                        <Image
-                            src="/images/phone-2.png"
-                            alt=""
-                            fill
-                            className="object-cover object-[50%_60%] group-hover:scale-105 transition duration-500"
-                        />
-                        <div className="absolute inset-0 bg-gray-400/10" />
-                        <div className="absolute inset-0 p-8 flex flex-col justify-between">
-                            <div>
-                                <h3 className="text-3xl font-semibold uppercase font-serif text-secondary">
-                                    PRODUCĂTORI
-                                </h3>
-                                <p className="mt-4 max-w-sm text-sm text-muted-foreground">
-                                    Introduci echipamente electrice și electronice pe piață?
-                                    Ai nevoie de un partener pentru conformare legală.
-                                </p>
-                            </div>
-                        </div>
-                    </Card>
+                {/* Card 2 */}
+                <Card className={'relative lg:row-span-2 overflow-hidden rounded-3xl group h-100 lg:h-full'}>
+                    <Image
+                        src={'/images/all-2.png'}
+                        alt={'Secțiune despre distribuitori'}
+                        fill
+                        className={'object-cover object-[50%_60%] group-hover:scale-105 transition duration-500'}
+                    />
+                    <div className="absolute inset-0 bg-gray-400/10" />
+                    <div className={'absolute inset-0 flex flex-col items-center px-8'}>
+                        <h3 className={'text-2xl md:text-4xl font-semibold uppercase pt-8 font-serif text-foreground'}>
+                            distribuitori
+                        </h3>
+                        <p className={'text-sm text-muted-foreground mt-2'}>
+                            Vinzi echipamente electrice direct către consumatori?
+                            Îți oferim soluții corecte de colectare DEEE.
+                        </p>
+                    </div>
+                </Card>
 
-                    {/* Card 2 */}
-                    <Card className="relative lg:row-span-2 overflow-hidden rounded-3xl group h-[400px] lg:h-full">
-                        <Image
-                            src="/images/all-2.png"
-                            alt=""
-                            fill
-                            className="object-cover object-[50%_60%] group-hover:scale-105 transition duration-500"
-                        />
-                        <div className="absolute inset-0 bg-gray-400/10" />
-                        <div className="absolute inset-0 p-8 flex flex-col justify-between">
-                            <div>
-                                <h3 className="text-3xl font-semibold uppercase  font-serif text-secondary">
-                                    DISTRIBUITORI
-                                </h3>
-                                <p className="mt-4 max-w-sm text-sm text-muted-foreground">
-                                    Vinzi echipamente electrice direct către consumatori?
-                                    Îți oferim soluții corecte de colectare DEEE.
-                                </p>
-                            </div>
-                        </div>
-                    </Card>
+                {/* Card 3 */}
+                <Card className={'relative lg:row-span-1 overflow-hidden rounded-3xl group h-100 lg:h-full'}>
+                    <Image
+                        src={'/images/rock.png'}
+                        alt={'Secțiune despre persoane fizice'}
+                        fill
+                        className={'object-cover object-[90%_80%] md:object-[50%_80%] scale-100 group-hover:scale-105 transition duration-500'}
+                    />
+                    <div className="absolute inset-0 bg-gray-400/10" />
+                    <div className={'absolute inset-0 flex flex-col items-center px-8'}>
+                        <h3 className={'text-2xl md:text-3xl font-semibold uppercase pt-8 font-serif text-foreground'}>
+                            persoane fizice
+                        </h3>
+                        <p className={'text-sm text-muted-foreground self-start  mt-2'}>
+                            Ai aparate vechi? Noi venim <br/> să le preluăm pentru reciclare.
+                        </p>
+                    </div>
+                </Card>
 
-                    {/* Card 3 */}
-                    <Card className="relative overflow-hidden rounded-3xl group h-[300px] lg:h-full">
-                        <Image
-                            src="/images/rock.png"
-                            alt=""
-                            fill
-                            className="object-cover object-[50%_95%] group-hover:scale-105 transition duration-500"
-                        />
-                        <div className="absolute inset-0 bg-gray-400/10" />
-                        <div className="absolute inset-0 p-8 flex flex-col justify-between">
-                            <div>
-                                <h3 className="text-3xl font-semibold uppercase  font-serif text-secondary">
-                                    PERSOANE FIZICE
-                                </h3>
-                                <p className="mt-3 text-sm text-muted-foreground">
-                                    Ai aparate vechi? Noi venim <br/> să le preluăm pentru reciclare.
-                                </p>
-                            </div>
-                        </div>
-                    </Card>
-
-                    {/* Card 4 */}
-                        <Card className="relative overflow-hidden rounded-3xl group h-[200px] lg:h-full hidden lg:block">
-                            <Image
-                                src="/images/plant.jpg"
-                                alt=""
-                                fill
-                                className="object-cover transition duration-500"
-                            />
-                            <div className="absolute inset-0 bg-gray-400/10" />
-                        </Card>
-
-                </div>
+                {/* Card 4 */}
+                <Card className={'relative lg:row-span-1 overflow-hidden rounded-3xl group h-50 lg:h-full'}>
+                    <Image
+                        src={'/images/plant.jpg'}
+                        alt={'Imagine decorativă cu o plantă'}
+                        fill
+                        className={'object-cover'}
+                    />
+                </Card>
             </div>
         </section>
-    );
+    )
 }
